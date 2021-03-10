9378348a27a2927feb464fc4a1b7b120605b8350
Kalys01 <kalysosmon96.g@gmail.com>
2021-03-10 16:51:47 +0600

    start of the project

---
+++ max_min.js
@@ -0,0 +1,5 @@
+var max = list => Math.max.apply(null, list);
+var min = list => Math.min.apply(null, list);
+
+console.log(max([-52, 56, 30, 29, -54, 0, -110]));
+console.log(min([-52, 56, 30, 29, -544, 0, -110]));
